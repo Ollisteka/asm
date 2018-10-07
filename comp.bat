55d005a200b3c4b94fd9821d62ee6d9aa95c1cec
Olga Zhukova <zhukova.o.m@yandex.ru>
2018-10-07 20:20:22 +0500

    Скрипты, компилирующие и запускающие код

---
+++ comp.bat
@@ -0,0 +1,18 @@
+@echo off
+if "%1" == "" goto :empty
+if "%1" == "/?" goto :help
+if exist %1
+tasm /m /l %1.asm /l
+tlink /t %1.obj
+goto :eof
+:help
+echo This script compiles and run .asm programm
+echo.
+echo Use this script as follow:
+echo comprun filename
+goto :eof
+:empty
+echo The argument is empty. Please, check the help
+echo comprun /?
+goto :eof
+:eof
