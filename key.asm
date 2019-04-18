726296ee291f1cd4dc9ee8739e0b7b0857797f16
Olga Zhukova <zhukova.o.m@yandex.ru>
2019-04-18 11:07:35 +0500

    Готовый обработчик 9 прерывания

--- key.asm
+++ key.asm
@@ -1,6 +1,8 @@
 .model tiny
 .code
 org 100h
+locals @@
+
 
 start:
 
@@ -13,32 +15,81 @@ ESCAPE = 1bh
 
 buffer db MAX_LEN, 0, MAX_LEN dup(0)
 output db 8 dup (0), CR, 0AH, '$'
+terminate db 0
 
 main:
-_loop:
-	xor ax, ax
-	int 16h
-	
-	cmp al, ESCAPE
-	je ex_it
-	
-	call reg_to_str
-	call append_char
-	call_print output
+	jmp init
 
-	jmp _loop
+	old_09h dd ?
+	BUF_LEN = 4
+	kb_buffer dw BUF_LEN dup(0)
+	head db 0
+	tail db 0
+
+new_09h:
+	write_to_tail:
+		push ax bx
+		xor ax, ax
+		in al, 60h
+		mov bx, offset kb_buffer
+		add bl, [tail]
+		mov [bx], ax
+		inc [tail]
+		
+		cmp [tail], BUF_LEN
+		jb @@skip_circling
+		mov byte ptr tail, 0
+
+	@@skip_circling:
+		cmp al, 1 ;ESCAPE
+		jne @@unlock
+		
+		mov [terminate], 1
 	
+	@@unlock:
+		in al, 61h 
+		push ax
+		or al, 80h  
+		out 61h, al ;set 7th bit and block
+		pop ax
+		out 61h, al ;restore
+
+		mov al, 20h  ;послать сигнал "конец прерывания" контроллеру прерываний 8259
+		out 20h,al
+
+		jmp .iret
+
+	.iret:
+		pop bx ax
+		iret
+	
+init:
+	call install
+_loop:
+	cmp [terminate], 0
+	jnz ex_it
+
+	xor bx, bx
+	mov bl, [head]
+	cmp bl, [tail]
+	je _loop
+	
+		add bx, offset kb_buffer
+		mov ax, [bx]
+		call reg_to_str
+		mov ah, PRINT_STR
+		mov dx, offset output
+		add dx, 3
+		int SYSCALL
+	inc [head]
+	cmp [head], BUF_LEN
+	jb _loop
+		mov [head], 0
+	jmp _loop
+
+
 ex_it:
+	call uninstall
 	call_exit
 	
-
-append_char:
-	cmp al, CR
-	je skip_enter
-		mov [di*1], al
-		ret
-	skip_enter:
-		mov dl, 0
-		mov [di*1], dl
-		ret
 end start
--- procs.asm
+++ procs.asm
@@ -22,3 +22,53 @@ rts2: mov [di], bl         ; store ASCII in buffer
     
 	inc di
     ret
+
+
+append_char:
+	cmp al, CR
+	je skip_enter
+		mov [di*1], al
+		ret
+	skip_enter:
+		mov dl, 0
+		mov [di*1], dl
+		ret
+
+
+install:
+	call save_vectors
+	mov dx, offset new_09h
+	call set_vector
+	ret
+
+
+uninstall:
+	mov dx, word ptr old_09h
+	push ds
+	push word ptr old_09h + 2
+	pop ds
+	call cs:set_vector
+	pop ds
+	ret
+
+
+save_vectors:
+	push es
+
+	push 0
+	pop es
+	mov bx, word ptr es:[09h * 4] ; загружаю адрес текущего вектора прерывания (смещение)
+	mov es, word ptr es:[09h * 4 + 2] ; загружаю адрес текущего вектора прерывания (сегмент)
+	
+	mov word ptr old_09h,   bx
+    mov word ptr old_09h+2, es
+	pop es
+	
+	ret
+	
+set_vector:
+	mov ah, 25h
+	mov al, 09h
+	int SYSCALL
+	ret
+
